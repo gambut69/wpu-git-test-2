61008c9140c5254d5a829f460fed5b2943033972
gambut69 <qrty4510@gmail.com>
2021-12-24 21:28:22 +0800

    Menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba Git Remote 2</title>
+</head>
+<body>
+    <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus, sit maxime magnam voluptatum tempora necessitatibus temporibus iusto similique cum neque beatae corporis libero in! Sapiente eaque quaerat mollitia cum sed.</p>
+</body>
+</html>
